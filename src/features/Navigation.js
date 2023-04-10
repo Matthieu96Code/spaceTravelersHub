58aa2d0a5c845dd3fa1e09359a9c4c311ccfef83
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-04-10 14:01:46 +0000

    create navigation feature

---
+++ src/features/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Space Traveler's Hub</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navigation
