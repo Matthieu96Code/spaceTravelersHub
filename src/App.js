4b56b04a175a57a1a407af6f1d6dcc267a64c06f
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-04-10 15:20:43 +0000

    create route for mission page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import Navigation from './features/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Outlet />
     </div>
   );
 }
