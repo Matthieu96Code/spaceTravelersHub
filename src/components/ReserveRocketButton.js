b74789967a8bea28c570d3c8eaab0989a3157323
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-11 14:09:45 +0000

    Fix the correct path of a component

--- src/components/ReserveRocketButton.js
+++ src/components/ReserveRocketButton.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { reserveRocket } from '../redux/rockets/rocketSlice';
 
-const ReserveRocketButton = (props) => {
+const ReservedRocketButton = (props) => {
   const { reservationId } = props;
   const dispatch = useDispatch();
 
   return <button type="button" className="btn btn-primary btn-sm" onClick={() => dispatch(reserveRocket({ reservationId }))}>Reserve Rocket</button>;
 };
 
-ReserveRocketButton.propTypes = {
+ReservedRocketButton.propTypes = {
   reservationId: PropTypes.string.isRequired,
 };
 
-export default ReserveRocketButton;
+export default ReservedRocketButton;
