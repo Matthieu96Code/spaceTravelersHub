b8f09666f81b29c2a15ffec0b150b08152ea0719
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-11 13:36:38 +0000

    Fix es linters error

--- src/components/ReserveRocketButton.js
+++ src/components/ReserveRocketButton.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { reserveRocket } from '../redux/rockets/rocketSlice';
 
-const reserveRocketButton = (props) => {
+const ReserveRocketButton = (props) => {
   const { reservationId } = props;
   const dispatch = useDispatch();
 
   return <button type="button" className="btn btn-primary btn-sm" onClick={() => dispatch(reserveRocket({ reservationId }))}>Reserve Rocket</button>;
 };
 
-reserveRocketButton.propTypes = {
+ReserveRocketButton.propTypes = {
   reservationId: PropTypes.string.isRequired,
 };
 
-export default reserveRocketButton;
+export default ReserveRocketButton;
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ export { default as JoinMissionButton } from './JoinMissionButton';
 export { default as InactiveBadge } from './InactiveBadge';
 export { default as ActiveBadge } from './ActiveBadge';
 
-export { default as ReservedRocketButton } from './ReserveRocketButton';
+export { default as ReserveRocketButton } from './ReserveRocketButton';
 export { default as CancelReservationButton } from './CancelReservationButton';
 export { default as ReservedBadge } from './ReservedBadge';
